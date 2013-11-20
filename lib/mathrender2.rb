1e4c7c34633fd899d7f183c3442e0d63dfb208ea
Ashish Sehra <asehra@gmail.com>
2013-11-20 14:30:31 +0000

    Added a mathml 2 svg renderer separately.

--- lib/mathrender2.rb
+++ lib/mathrender2.rb
@@ -9,15 +9,19 @@ module Mathrender2
     def render_png(mathml, file_path='sample.png')
       path = File.join(File.dirname(__FILE__), 'tmp', "math_#{ Time.now.to_i }#{ rand(100) }.png")
 
-      output = Phantomjs.run File.join(File.dirname(__FILE__), 'mathrender2', 'jax.js'), mathml, path
+      output = Phantomjs.run File.join(File.dirname(__FILE__), 'mathrender2', 'mathml2png.js'), mathml, path
       png_data = File.read(path)
       File.delete(path)
       png_data
+    end
+
+    def render_svg(mathml)
+      Phantomjs.run File.join(File.dirname(__FILE__), 'mathrender2', 'mathml2svg.js'), mathml
     end
 
   end
 end
 
 
-# math = '<math display="block"><mrow><mi>x</mi><mo>=</mo><mfrac><mrow><mo>−</mo><mi>b</mi><mo>±</mo><msqrt><mrow><msup><mi>b</mi><mn>2</mn></msup><mo>−</mo><mn>4</mn><mi>a</mi><mi>c</mi></mrow></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></mrow></math>'
-# puts Mathrender2.render_png(math, 'lib/sample2.png')
+math = '<math display="block"><mrow><mi>x</mi><mo>=</mo><mfrac><mrow><mo>−</mo><mi>b</mi><mo>±</mo><msqrt><mrow><msup><mi>b</mi><mn>2</mn></msup><mo>−</mo><mn>4</mn><mi>a</mi><mi>c</mi></mrow></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></mrow></math>'
+puts Mathrender2.render_svg(math)
