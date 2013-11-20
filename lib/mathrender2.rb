691b389666055dcc4d785030be8c2e45b515ad90
Ashish Sehra <asehra@gmail.com>
2013-11-20 12:07:56 +0000

    Rendering raw png output.

--- lib/mathrender2.rb
+++ lib/mathrender2.rb
@@ -7,7 +7,12 @@ module Mathrender2
   class << self
 
     def render_png(mathml, file_path='sample.png')
-      Phantomjs.run File.join(File.dirname(__FILE__), 'mathrender2', 'jax.js'), mathml, file_path
+      path = File.join(File.dirname(__FILE__), 'tmp', "math_#{ Time.now.to_i }#{ rand(100) }.png")
+
+      output = Phantomjs.run File.join(File.dirname(__FILE__), 'mathrender2', 'jax.js'), mathml, path
+      png_data = File.read(path)
+      File.delete(path)
+      png_data
     end
 
   end
@@ -15,4 +20,4 @@ end
 
 
 # math = '<math display="block"><mrow><mi>x</mi><mo>=</mo><mfrac><mrow><mo>−</mo><mi>b</mi><mo>±</mo><msqrt><mrow><msup><mi>b</mi><mn>2</mn></msup><mo>−</mo><mn>4</mn><mi>a</mi><mi>c</mi></mrow></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></mrow></math>'
-# Mathrender2.render_png(math, 'lib/sample2.png')
+# puts Mathrender2.render_png(math, 'lib/sample2.png')
